35adadbd4247e05556e55165391fc50eb07503df
Yuichiro Kikura <kikurage@YuichironoMacBook-Pro.local>
2023-11-18 17:10:26 +0900

    add doc

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,56 @@
 import { createRoot } from 'react-dom/client';
-import { AppShell } from './AppShell';
+import { useState } from 'react';
+import { VirtualListView } from './VirtualList/VirtualListView';
 
 window.addEventListener('DOMContentLoaded', () => {
     createRoot(document.getElementById('root')!).render(<AppShell />);
 });
+
+export const AppShell = () => {
+    const [rows, setRows] = useState(1000);
+
+    return (
+        <div>
+            <header
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '16px',
+                    marginBottom: '16px',
+                }}
+            >
+                <span>行数</span>
+                <input type="number" value={rows} onChange={(ev) => setRows(Number(ev.target.value))} />
+            </header>
+            <div
+                style={{
+                    border: '2px solid #000',
+                    resize: 'both',
+                    overflow: 'clip',
+                    background: '#aaa',
+                    height: '600px',
+                }}
+            >
+                <VirtualListView rows={rows} ItemRenderer={ItemRenderer} />
+            </div>
+        </div>
+    );
+};
+
+const ItemRenderer = ({ row }: { row: number }) => {
+    return (
+        <div
+            style={{
+                background: '#bcf5ff',
+                borderBottom: '1px solid #000',
+                resize: 'vertical',
+                overflow: 'auto',
+                padding: '4px 16px',
+                boxSizing: 'border-box',
+                height: '50px',
+            }}
+        >
+            Item: {row}
+        </div>
+    );
+};
